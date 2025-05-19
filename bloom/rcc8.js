6767216254b893a3ace9a78d5474ea2ffc86d513
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-05-19 19:34:08 -0400

    Updating composition table

--- bloom/rcc8.js
+++ bloom/rcc8.js
@@ -1,87 +1,120 @@
 
-export const RCC8Relations = ["DC", "EC", "PO", "EQ", "TPP", "NTPP", "TPPi", "NTPPi"];
+const DC = "DC"; // Disconnected
+const EC = "EC"; // Externally Connected
+const PO = "PO"; // Partial Overlap
+const EQ = "EQ"; // Equal
+const TPP = "TPP"; // Tangential Proper Part
+const NTPP = "NTPP"; // Non-Tangential Proper Part
+const TPPi = "TPPi"; // Tangential Proper Part inverse
+const NTPPi = "NTPPi"; // Non-Tangential Proper Part inverse
 
+export const RCC8Relations = [
+  DC,
+  EC,
+  PO,
+  EQ,
+  TPP,
+  NTPP,
+  TPPi,
+  NTPPi
+];
+
+
+export function converseRelation(relation) {
+  switch (relation) {
+    case DC: return DC;
+    case EC: return EC;
+    case PO: return PO;
+    case EQ: return EQ;
+    case TPP: return TPPi;
+    case NTPP: return NTPPi;
+    case TPPi: return TPP;
+    case NTPPi: return NTPP;
+    default: throw new Error(`Unknown relation: ${relation}`);
+  }
+}
 
 // Full RCC8 composition table
 export const RCC8CompositionTable = {
   DC: {
-    DC: ["DC"],
-    EC: ["DC", "EC"],
-    PO: ["DC", "PO"],
-    EQ: ["DC"],
-    TPP: ["DC"],
-    NTPP: ["DC"],
-    TPPi: ["DC"],
-    NTPPi: ["DC"]
+    DC: RCC8Relations,
+    EC: [DC, EC, PO, TPP, NTPP],
+    PO: [DC, EC, PO, TPP, NTPP],
+    TPP: [DC, EC, PO, TPP, NTPP],
+    NTPP: [DC, EC, PO, TPP, NTPP],
+    TPPi : [DC],
+    NTPPi: [DC],
+    EQ: [DC]
   },
-  EC: {
-    DC: ["DC", "EC"],
-    EC: ["DC", "EC", "PO"],
-    PO: ["EC", "PO"],
-    EQ: ["EC"],
-    TPP: ["EC"],
-    NTPP: ["EC"],
-    TPPi: ["EC"],
-    NTPPi: ["EC"]
+  EC : {
+    DC: [DC, EC, PO, TPPi, NTPPi],
+    EC: [DC, EC, PO, TPP, TPPi, EQ	],
+    PO: [DC, EC, PO, TPP, NTPP	],
+    TPP: [EC, PO, TPP, NTPP	],
+    NTPP: [PO, TPP, NTPP	],
+    TPPi : [DC, EC	],
+    NTPPi: [DC],
+    EQ: [EC]
   },
-  PO: {
-    DC: ["DC", "PO"],
-    EC: ["EC", "PO"],
-    PO: ["DC", "EC", "PO", "TPP", "NTPP", "TPPi", "NTPPi"],
-    EQ: ["PO"],
-    TPP: ["PO", "TPP", "NTPP"],
-    NTPP: ["PO", "NTPP"],
-    TPPi: ["PO", "TPPi", "NTPPi"],
-    NTPPi: ["PO", "NTPPi"]
+  PO : {
+    DC: [DC, EC, PO, TPPi, NTPPi	],
+    EC: [DC, EC, PO, TPPi, NTPPi	],
+    PO: RCC8Relations,
+    TPP: [PO, TPP, NTPP	],
+    NTPP: [PO, TPP, NTPP	],
+    TPPi : [DC, EC, PO, TPPi, NTPPi	],
+    NTPPi: [DC, EC, PO, TPPi, NTPPi	],
+    EQ: [PO]
   },
-  EQ: {
-    DC: ["DC"],
-    EC: ["EC"],
-    PO: ["PO"],
-    EQ: ["EQ"],
-    TPP: ["TPP"],
-    NTPP: ["NTPP"],
-    TPPi: ["TPPi"],
-    NTPPi: ["NTPPi"]
+  TPP : {
+    DC: [DC],
+    EC: [DC, EC	],
+    PO: [DC, EC, PO, TPP, NTPP	],
+    TPP: [TPP, NTPP	],
+    NTPP: [NTPP],
+    TPPi : [DC, EC, PO, TPP, TPPi, EQ	],
+    EQ: [TPP],
+    NTPPi: [DC, EC, PO, TPPi, NTPPi	]
   },
-  TPP: {
-    DC: ["DC"],
-    EC: ["EC"],
-    PO: ["PO", "TPP", "NTPP"],
-    EQ: ["TPP"],
-    TPP: ["TPP", "NTPP"],
-    NTPP: ["NTPP"],
-    TPPi: ["PO", "TPP", "NTPP"],
-    NTPPi: ["PO", "NTPP"]
+  NTPP : {
+    DC: [DC],
+    EC: [DC],
+    PO: [DC, EC, PO, TPP, NTPP	],
+    TPP: [NTPP],
+    NTPP: [NTPP],
+    TPPi : [DC, EC, PO, TPP, NTPP	],
+    NTPPi: RCC8Relations,
+    EQ: [NTPP]
   },
-  NTPP: {
-    DC: ["DC"],
-    EC: ["EC"],
-    PO: ["PO", "NTPP"],
-    EQ: ["NTPP"],
-    TPP: ["NTPP"],
-    NTPP: ["NTPP"],
-    TPPi: ["PO", "NTPP"],
-    NTPPi: ["PO", "NTPP"]
+  TPPi : {
+    DC: [DC, EC, PO, TPPi, NTPPi	],
+    EC: [EC, PO, TPPi, NTPPi	],
+    PO: [PO, TPPi, NTPPi	],
+    TPP: [PO, TPP, TPPi, EQ	],
+    NTPP: [PO, TPP, NTPP	],
+    TPPi : [TPPi, NTPPi	],
+    NTPPi: [NTPPi],
+    EQ: [TPPi]
   },
-  TPPi: {
-    DC: ["DC"],
-    EC: ["EC"],
-    PO: ["PO", "TPPi", "NTPPi"],
-    EQ: ["TPPi"],
-    TPP: ["PO", "TPPi", "NTPPi"],
-    NTPP: ["PO", "NTPPi"],
-    TPPi: ["TPPi", "NTPPi"],
-    NTPPi: ["NTPPi"]
+  NTPPi : {
+    DC: [DC, EC, PO, TPPi, NTPPi	],
+    EC: [PO, TPPi, NTPPi	],
+    PO: [PO, TPPi, NTPPi	],
+    TPP: [PO, TPPi, NTPPi	],
+    NTPP: [PO, TPP, NTPP, TPPi, NTPPi, EQ	],
+    TPPi : [NTPPi	],
+    NTPPi: [NTPPi],
+    EQ: [NTPPi]
   },
-  NTPPi: {
-    DC: ["DC"],
-    EC: ["EC"],
-    PO: ["PO", "NTPPi"],
-    EQ: ["NTPPi"],
-    TPP: ["PO", "NTPPi"],
-    NTPP: ["PO", "NTPPi"],
-    TPPi: ["NTPPi"],
-    NTPPi: ["NTPPi"]
+  EQ : {
+    DC: [DC],
+    EC: [EC],
+    PO: [PO],
+    TPP: [TPP],
+    NTPP: [NTPP],
+    TPPi : [TPPi ],
+    NTPPi: [NTPPi],
+    EQ: [EQ]
   }
+  
 };
