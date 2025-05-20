53951a04fd82b3ff9a29f93695ad0a3f098be9fe
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-05-20 14:19:25 -0400

    Sort of by exclusion?

--- bloom/visbyconstraint.js
+++ bloom/visbyconstraint.js
@@ -1,51 +1,17 @@
 import * as bloom from "https://penrose.cs.cmu.edu/bloom.min.js";
-import {DC, EC, PO, EQ, TPP, NTPP, TPPi, NTPPi} from "./rcc8.js";
 const minOverlap = 5; // This could be a percentage
 
 
+// minOverlap for connecting
+
+// maxOverlap for TOUCHING = 1
 
 
-function setKey(rels) {
-  // rels: Array or Set of relation strings
-  return Array.from(rels).sort().join(",");
-}
-
-function equalSets(setA, setB) {
-    if (setA.size !== setB.size) return false;
-    for (const item of setA) {
-        if (!setB.has(item)) return false;
-    }
-    return true;
-}
-
-/** I think this is the viz insight -- deformation can only happen in some scenarios... */
-
-// And I think it's crucial to mention that these are 
-// specifically for R^2. There are more abstract ways to do this in other spaces / in the  abstract.
-const RCC6Regions = {};
-
-RCC6Regions[setKey([NTTP, TPP])] = "PP"; // Proper Subset
-RCC6Regions[setKey([DC, EC])] = "NOINT"; // No intersection
-RCC6Regions[setKey([EC, PO])] = "EC_PO"; // TouchingEdge Not Subset
-RCC6Regions[setKey([PO, TPP])] = "PO_TPP"; // Touching Edge, Proper Subset
-RCC6Regions[setKey([TPP, EQ])] = "TPP_EQ"; // TOuching Edge, Subset
 
 
-RCC6Regions[setKey([DC, EC, PO])] = "NOTSUBSET";
-RCC6Regions[setKey([EC, PO, TPP])] = "TOUCHING_NEQ";
-RCC6Regions[setKey([PO, TPP, EQ])] = "SOME_INTERSECTION_TOUCHING";
 
-RCC6Regions[setKey([DC, EC, PO, TPP])] = "NOT_EQUAL_NOT_NTTP";
-RCC6Regions[setKey([EC, PO, TPP, EQ])] = "TOUCHING_OVERLAP";
-RCC6Regions[setKey([PO, TPP, EQ, NTPP])] = "SOME_INTERSECTION"; 
+// eesh, maybe the correct approach ~is~ to write !EC, !DC, etc
 
-
-RCC6Regions[setKey([DC, EC, PO, TPP, EQ])] = "TOUCHING_EDGE_OR_DC";
-RCC6Regions[setKey([DC, EC, PO, NTPP, EQ])] = "NOT_TPP";
-RCC6Regions[setKey([DC, EC, PO, TPP, NTPP])] = "NOT_EQ";
-RCC6Regions[setKey([EC, PO, TPP, NTPP, EQ])] = "NOT_DC";
-
-RCC6Regions[setKey(RCC6Relations)] = "U";
 
 
 /**
@@ -149,14 +115,15 @@ export async function buildDiagram(rcc8util) {
   const PO = predicate();
   const EQ = predicate();
   const TPP = predicate();
-  const TPPi = predicate();
   const NTPP = predicate();
-  const NTPPi = predicate();
-
-  // Connected Pairs //
 
 
-  /// SOme other regions ///
+  const notDC = predicate();
+  const notEC = predicate();
+  const notPO = predicate();
+  const notEQ = predicate();
+  const notTPP = predicate();
+  const notNTPP = predicate();
 
 
   let regionMap = {};
@@ -191,51 +158,87 @@ export async function buildDiagram(rcc8util) {
     for (const b of Object.keys(relations[a])) {
         const rels = relations[a][b]; // array of allowed relations
 
+
+
+
         if(a === b) {
             continue;
         }
 
+          let aRegion = regionMap[a];
+          let bRegion = regionMap[b];
 
-        // The thing is, rels could be **anything**.
+        // If rels has only one relation, use that
+        
 
-        let contiguousSets = rcc8util.partitionIntoContiguousSets(rels);
+        // Otherwise, if there are multiple relations, exclude the ones that
+        // are not in the list
 
-        // HACK, DOES NOT WORK. For now,
-        // no support for disjunction.
-        // Check if rels is a contiguous RCC8 set.
-
-        for (const rel of rels) {
-
-            const A = regionMap[a], B = regionMap[b];
-            if (!A || !B) continue;
-            if (rel === "EQ") EQ(A, B);
-            if (rel === "DC") DC(A, B);
-            if (rel === "EC") EC(A, B);
-            if (rel === "PO") PO(A, B);
-            if (rel === "TPP") TPP(A, B);
-            if (rel === "TPPi") TPPi(A, B);
-            if (rel === "NTPP") NTPP(A, B);
-            if (rel === "NTPPi") NTPPi(A, B);
+        if (rels.length === 1) {
+          let rel = rels[0];
+          if (rel === "DC") DC(aRegion, bRegion);
+          else if (rel === "EC") EC(aRegion, bRegion);
+          else if (rel === "PO") PO(aRegion, bRegion);
+          else if (rel === "EQ") EQ(aRegion, bRegion);
+          else if (rel === "TPP") TPP(aRegion, bRegion);
+          else if (rel === "NTPP") NTPP(aRegion, bRegion);
+        
         }
-        
-        
+        else if (rels.length > 1) {
+          // Exclude the relations that are not in the list
+          let relsSet = new Set(rels);
+          if (!relsSet.has("DC")) notDC(aRegion, bRegion);
+
+          if (!relsSet.has("EC")) notEC(aRegion, bRegion);
+
+          if (!relsSet.has("PO")) notPO(aRegion, bRegion);
+
+          if (!relsSet.has("EQ")) notEQ(aRegion, bRegion);
+          if (!relsSet.has("TPP")) notTPP(aRegion, bRegion);
+          if (!relsSet.has("NTPP")) notNTPP(aRegion, bRegion);
+        }
 
     }
 }
 
 
-  // RCC8 constraints
+  //// TODO: Probably need to write these in a more consistent way :)
+
 
   // DC: Disconnected
   forallWhere({ a: Region, b: Region }, ({ a, b }) => DC.test(a, b), ({ a, b }) => {
     ensure(disjoint(a.icon, b.icon, minOverlap));
   });
 
+
+  // NOT DC
+  forallWhere({ a: Region, b: Region }, ({ a, b }) => notDC.test(a, b), ({ a, b }) => {
+    overlapping(a.icon, b.icon); // should do this with radii and centers
+  });
+
+
+
   // EC: Externally Connected
   forallWhere({ a: Region, b: Region }, ({ a, b }) => EC.test(a, b), ({ a, b }) => {
     ensure(touching(a.icon, b.icon));
     ensure(disjoint(a.icon, b.icon));
   });
+
+
+// NOT EC
+forallWhere({ a: Region, b: Region }, ({ a, b }) => notEC.test(a, b), ({ a, b }) => {
+  ensure(
+    greaterThan(
+      bloom.abs(
+        bloom.sub(
+          bloom.ops.vdist(a.icon.center, b.icon.center),
+          bloom.add(a.icon.r, b.icon.r)
+        )
+      ),
+      epsilon
+    )
+  );
+});
 
 
   // PO: Partial Overlap
@@ -262,12 +265,43 @@ export async function buildDiagram(rcc8util) {
     );
   });
 
+
+  
+forallWhere({ a: Region, b: Region }, ({ a, b }) => notPO.test(a, b), ({ a, b }) => {
+  // Enforce: d ≤ |r1 - r2| + ε  OR  d ≥ r1 + r2 - ε
+  // (This will "push" the solution out of the PO interval)
+  ensure(
+    bloom.or(
+      lessThan(
+        bloom.ops.vdist(a.icon.center, b.icon.center),
+        bloom.add(bloom.abs(bloom.sub(a.icon.r, b.icon.r)), epsilon)
+      ),
+      greaterThan(
+        bloom.ops.vdist(a.icon.center, b.icon.center),
+        bloom.sub(bloom.add(a.icon.r, b.icon.r), epsilon)
+      )
+    )
+  );
+});
+
+
   // EQ: Equal
   forallWhere({ a: Region, b: Region }, ({ a, b }) => EQ.test(a, b), ({ a, b }) => {
     ensure(equal(a.icon.r, b.icon.r));
     ensure(contains(a.icon, b.icon));
     ensure(contains(b.icon, a.icon));
   });
+
+  // Sort of works I think?
+  forallWhere({ a: Region, b: Region }, ({ a, b }) => notEQ.test(a, b), ({ a, b }) => {
+  ensure(
+    greaterThan(
+      bloom.ops.vdist(a.icon.center, b.icon.center),
+      epsilon
+    )
+  );});
+
+
 
   // TPP: Tangential Proper Part
   forallWhere({ a: Region, b: Region }, ({ a, b }) => TPP.test(a, b), ({ a, b }) => {
@@ -282,6 +316,28 @@ export async function buildDiagram(rcc8util) {
     layer(a.icon, b.icon);
   });
 
+
+  forallWhere({ a: Region, b: Region }, ({ a, b }) => notTPP.test(a, b), ({ a, b }) => {
+    ensure(
+      greaterThan(
+        bloom.ops.vdist(a.icon.center, b.icon.center),
+        bloom.add(bloom.abs(bloom.sub(a.icon.r, b.icon.r)), epsilon)
+      )
+    );
+  });
+
+
+
+forallWhere({ a: Region, b: Region }, ({ a, b }) => notNTPP.test(a, b), ({ a, b }) => {
+  ensure(
+    greaterThan(
+      bloom.ops.vdist(a.icon.center, b.icon.center),
+      bloom.sub(bloom.abs(b.icon.r, a.icon.r), epsilon)
+    )
+  );
+});
+
+
   // NTPP: Non-Tangential Proper Part
   forallWhere({ a: Region, b: Region }, ({ a, b }) => NTPP.test(a, b), ({ a, b }) => {
     ensure(contains(b.icon, a.icon, 5));
@@ -289,7 +345,6 @@ export async function buildDiagram(rcc8util) {
     layer(a.icon, b.icon);
   });
 
-  // And composite regions
 
 
   // Contains, but not equal
